46939b9998263c0712573cab2530899eff4d90f9
Alexis J. Villegas <alexis@ajvillegas.com>
2020-07-15 19:49:25 -0700

    Update syntax and remove Gravity Forms mods

--- inc/integrations.php
+++ inc/integrations.php
@@ -15,25 +15,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/** Gravity Forms */
-
-// Enable Gravity Forms label visibility settings.
-add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
-
-add_filter( 'gform_validation_message', 'ajv_proto_gform_validation_message', 10, 2 );
-/**
- * Modify Gravity Forms validation message.
- *
- * @since 1.0.0
- * @param string $message The validation message to be filtered.
- * @param array  $form The current form.
- */
-function ajv_proto_gform_validation_message( $message, $form ) {
-
-	return '';
-
-}
-
 /** Jetpack */
 
 add_action( 'after_setup_theme', 'ajv_proto_jetpack_setup' );
@@ -48,31 +29,37 @@ add_action( 'after_setup_theme', 'ajv_proto_jetpack_setup' );
 function ajv_proto_jetpack_setup() {
 
 	// Add theme support for Infinite Scroll.
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'render'    => 'ajv_proto_infinite_scroll_render',
-		'footer'    => 'page',
-	) );
+	add_theme_support(
+		'infinite-scroll',
+		array(
+			'container' => 'main',
+			'render'    => 'ajv_proto_infinite_scroll_render',
+			'footer'    => 'page',
+		)
+	);
 
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
 
 	// Add theme support for Content Options.
-	add_theme_support( 'jetpack-content-options', array(
-		'post-details'    => array(
-			'stylesheet' => 'ajv-proto-style',
-			'date'       => '.posted-on',
-			'categories' => '.cat-links',
-			'tags'       => '.tags-links',
-			'author'     => '.byline',
-			'comment'    => '.comments-link',
-		),
-		'featured-images' => array(
-			'archive' => true,
-			'post'    => true,
-			'page'    => true,
-		),
-	) );
+	add_theme_support(
+		'jetpack-content-options',
+		array(
+			'post-details'    => array(
+				'stylesheet' => 'ajv-proto-style',
+				'date'       => '.posted-on',
+				'categories' => '.cat-links',
+				'tags'       => '.tags-links',
+				'author'     => '.byline',
+				'comment'    => '.comments-link',
+			),
+			'featured-images' => array(
+				'archive' => true,
+				'post'    => true,
+				'page'    => true,
+			),
+		)
+	);
 
 }
 
