ff357f3efd4dbd8ae4f062e144e47110a396747e
Alexis J. Villegas <alexis@ajvillegas.com>
2020-09-07 19:39:20 -0700

    Update compiled editor scripts

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -1,26 +1,179 @@
-/**
- * JS functionality for the block editor.
- *
- * @package    AJV_Proto
- * @author     Alexis J. Villegas
- * @link       http://www.alexisvillegas.com
- * @license    GPL-2.0+
- */
+( function( blocks, editor, element ) {
+	const __ = wp.i18n.__;
+	const el = element.createElement;
+	const registerBlockType = blocks.registerBlockType;
+	const { BlockControls, InspectorControls, AlignmentToolbar, PanelColorSettings } = editor;
+	const { Fragment } = element;
+
+	registerBlockType( 'ajv-proto/colored-line', {
+		title: __( 'Colored Line', 'ajv-proto' ),
+		icon: 'minus',
+		category: 'common',
+		keywords: [ 'ajv-proto', 'colored', 'line' ],
+		attributes: {
+			alignment: {
+				type: 'string',
+				default: 'left'
+			},
+			lineColor: {
+				type: 'string',
+				default: '#ff7f50'
+			}
+		},
+		example: {
+			attributes: {
+				alignment: 'left',
+				lineColor: '#ff7f50'
+			}
+		},
+
+		edit: ( function( props ) {
+			const onChangeAlignment = ( newAlignment ) => {
+				props.setAttributes({ alignment: newAlignment === undefined ? 'left' : newAlignment });
+			};
+			const blockParentStyle = {
+				width: '100%',
+				padding: '0',
+				marginTop: '0',
+				marginBottom: '0',
+				textAlign: props.attributes.alignment
+			};
+			const blockInnerStyle = {
+				backgroundColor: props.attributes.lineColor,
+				width: '100%',
+				height: '2px',
+				display: 'inline-block'
+			};
+			const colorSamples = [
+				{
+					name: 'Coral',
+					slug: 'coral',
+					color: '#FF7F50'
+				},
+				{
+					name: 'Brown',
+					slug: 'brown',
+					color: '#964B00'
+				},
+				{
+					name: 'Purple',
+					slug: 'purple',
+					color: '#800080'
+				}
+			];
+
+			return [
+				el(
+					Fragment,
+					{ key: 'fragment' },
+					el(
+						InspectorControls,
+						{},
+						el(
+							PanelColorSettings,
+							{
+								title: 'Color Settings',
+								initialOpen: true,
+								colorSettings: [
+									{
+										colors: colorSamples,
+										value: props.attributes.lineColor,
+										label: 'Line color',
+										onChange: ( value ) => {
+											props.setAttributes(
+												{ lineColor: value }
+											);
+										}
+									}
+								]
+							}
+						)
+					)
+				),
+				el(
+					BlockControls,
+					{ key: 'controls' },
+					el(
+						AlignmentToolbar,
+						{
+							value: props.attributes.alignment,
+							onChange: onChangeAlignment
+						}
+					)
+				),
+				el(
+					'div',
+					{
+						key: 'colored-line',
+						className: props.className
+					},
+					el(
+						'div',
+						{
+							className: 'ajv-proto-colored-line',
+							style: blockParentStyle
+						},
+						el(
+							'span',
+							{ style: blockInnerStyle }
+						)
+					)
+				)
+			];
+		}),
+
+		save: function( props ) {
+			const blockParentStyle = {
+				width: '100%',
+				padding: '0',
+				marginTop: '0',
+				marginBottom: '0',
+				textAlign: props.attributes.alignment
+			};
+			const blockInnerStyle = {
+				backgroundColor: props.attributes.lineColor,
+				width: '100%',
+				height: '2px',
+				display: 'inline-block'
+			};
+
+			return (
+				el(
+					'div',
+					{ className: props.className },
+					el(
+						'div',
+						{
+							className: 'ajv-proto-colored-line',
+							style: blockParentStyle
+						},
+						el(
+							'span',
+							{ style: blockInnerStyle }
+						)
+					)
+				)
+			);
+		}
+	});
+}(
+	window.wp.blocks,
+	window.wp.blockEditor,
+	window.wp.element,
+	window.wp.components
+) );
 
 wp.domReady( () => {
-	// Unregister core blocks.
+
 	wp.blocks.unregisterBlockType( 'core/verse' );
 
-	// Remove block styles.
 	wp.blocks.unregisterBlockStyle(
 		'core/quote',
 		[ 'large' ]
 	);
 
-	// Start in a checked state.
-	var checked = true;
+	let checked = true;
 
-	// Trigger side effect after block editor is done saving meta box values.
 	wp.data.subscribe( () => {
 		const isSavingMetaBoxes = wp.data.select( 'core/edit-post' ).isSavingMetaBoxes();
 
@@ -28,35 +181,57 @@ wp.domReady( () => {
 			checked = false;
 		} else {
 			if ( ! checked ) {
-				// Get the meta box input.
-				const metaboxInput = document.querySelector( '#ajv-proto-layout-meta-box .selected input[type="radio"]' );
-				// If meta box input, get its value.
-				const metaboxValue = metaboxInput ? metaboxInput.value : 'default-layout';
-				// Get the meta value from the database.
-				const databaseValue = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'meta')['_ajv_proto_post_layout'];
 
-				// Check if values are different.
+				const metaboxInput = document.querySelector( '#ajv-proto-layout-meta-box .selected input[type="radio"]' );
+
+				const metaboxValue = metaboxInput ? metaboxInput.value : 'default-layout';
+
+				const databaseValue = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'meta' )._ajv_proto_post_layout;
+
 				if ( metaboxValue !== databaseValue ) {
-					// Reload the page from the server.
+
 					window.location.reload( true );
 				}
 
 				checked = true;
 			}
 		}
-	} );
+	});
 
-	// Trigger side effect after block editor is done saving the post.
-	/* wp.data.subscribe( () => {
-		const isSavingPost = wp.data.select( 'core/editor' ).isSavingPost();
+});
 
-		if ( isSavingPost ) {
-			checked = false;
-		} else {
-			if ( ! checked ) {
-				console.log( 'Done saving post.' );
-				checked = true;
-			}
-		}
-	} ); */
-} );
+wp.domReady( () => {
+
+	const el = wp.element.createElement;
+	const Fragment = wp.element.Fragment;
+	const PluginSidebar = wp.editPost.PluginSidebar;
+	const PluginSidebarMoreMenuItem = wp.editPost.PluginSidebarMoreMenuItem;
+	const registerPlugin = wp.plugins.registerPlugin;
+	const moreIcon = 'layout';
+
+	function Component() {
+		return el(
+			Fragment,
+			{},
+			el(
+				PluginSidebarMoreMenuItem,
+				{
+					target: 'sidebar-name'
+				},
+				'Layout Settings'
+			),
+			el(
+				PluginSidebar,
+				{
+					name: 'sidebar-name',
+					title: 'Layout Settings'
+				},
+				'Content of the sidebar'
+			)
+		);
+	}
+	registerPlugin( 'plugin-name', {
+		icon: moreIcon,
+		render: Component
+	});
+});
