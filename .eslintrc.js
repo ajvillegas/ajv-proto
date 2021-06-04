93add97ee232470bab18780380be0a39007227de
Alexis J. Villegas <alexis@ajvillegas.com>
2021-06-03 21:34:22 -0700

    Update syntax

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,16 +5,16 @@ module.exports = {
 		es6: true,
 		node: true
 	},
-	extends: ['eslint:recommended', 'wordpress'],
+	extends: [ 'eslint:recommended', 'wordpress' ],
 	parserOptions: {
 		sourceType: 'module'
 	},
 	rules: {
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-		'newline-after-var': ['error', 'always'],
-		'no-multiple-empty-lines': ['error', {'max': 1, 'maxBOF': 0}]
+		indent: [ 'error', 'tab' ],
+		'linebreak-style': [ 'error', 'unix' ],
+		quotes: [ 'error', 'single' ],
+		semi: [ 'error', 'always' ],
+		'newline-after-var': [ 'error', 'always' ],
+		'no-multiple-empty-lines': [ 'error', {'max': 1, 'maxBOF': 0} ]
 	}
 };
