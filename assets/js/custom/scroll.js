b1164b431ebd3094f0289b925121ee08fa3aff1e
Alexis J. Villegas <alexis@ajvillegas.com>
2021-06-08 12:48:47 -0700

    Wrap script inside an IIFE

--- assets/js/custom/scroll.js
+++ assets/js/custom/scroll.js
@@ -7,38 +7,38 @@
  * @license    GPL-2.0+
  */
 
-function scrollToTop() {
-	const element = document.querySelector( 'body' ).offsetTop;
+( function() {
+	function scrollToTop() {
+		const element = document.querySelector( 'body' ).offsetTop;
 
-	window.scroll({
-		top: element,
-		left: 0,
-		behavior: 'smooth'
-	});
-}
-
-function scrollClasses() {
-	const topButton = document.querySelector( '.to-top' );
-
-	// When the user scrolls down 100px from the top of the document.
-	if ( 100 < document.body.scrollTop || 100 < document.documentElement.scrollTop ) {
-
-		// Display scroll button.
-		topButton.style.display = 'block';
-	} else {
-
-		// Hide scroll button.
-		topButton.style.display = 'none';
+		window.scroll({
+			top: element,
+			left: 0,
+			behavior: 'smooth'
+		});
 	}
-}
 
-// When the DOM is loaded, add click handler to scroll button.
-window.addEventListener( 'load', function() {
-	scrollClasses();
-	document.querySelector( '.to-top' ).addEventListener( 'click', scrollToTop );
-});
+	function scrollClasses() {
+		const topButton = document.querySelector( '.to-top' );
 
-// When the user scrolls, call function to toggle classes.
-window.onscroll = function() {
+		// When the user scrolls down 100px from the top of the document.
+		if ( 100 < document.body.scrollTop || 100 < document.documentElement.scrollTop ) {
+
+			// Display scroll button.
+			topButton.style.display = 'block';
+		} else {
+
+			// Hide scroll button.
+			topButton.style.display = 'none';
+		}
+	}
+
+	// Add click handler to scroll button.
 	scrollClasses();
-};
+	document.querySelector( '.to-top' ).addEventListener( 'click', scrollToTop, false );
+
+	// When the user scrolls, call function to toggle classes.
+	window.onscroll = function() {
+		scrollClasses();
+	};
+}() );
